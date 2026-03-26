80048e75368b6acadb61769e2ff6f9954bdf6398
Keerthi98517 <keerthana.bn@pacedigitek.com>
2026-03-26 10:37:41 +0530

    Update approve.js

--- netlify/functions/approve.js
+++ netlify/functions/approve.js
@@ -2,23 +2,33 @@ exports.handler = async function(event, context) {
   const refNum = event.queryStringParameters.refNum;
   const action = event.queryStringParameters.action;
   
-  const apiURL = "https://www.zohoapis.in/creator/custom/admin_pacedigitek/process_visitor_approval?refNum=" + refNum + "&action=" + action;
+  const apiURL = "https://www.zohoapis.in/creator/custom/admin_pacedigitek/process_visitor_approval?public_key=8xJTrKj9bKGAyOaPqhZ6rGBDK&refNum=" + refNum + "&action=" + action;
   
-  const response = await fetch(apiURL, {
-    method: "GET",
-    headers: {
-      "X-ZC-Public-Key": "8xJTrKj9bKGAyOaPqhZ6rGBDK"
-    }
-  });
-  
-  const data = await response.json();
-  
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  };
+  try {
+    const response = await fetch(apiURL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    
+    const data = await response.json();
+    
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+  } catch(error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({ error: error.message })
+    };
+  }
 };
